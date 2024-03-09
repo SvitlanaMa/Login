d3a4bdd490932744e71514ae26d09b043559648b
Svitlana <fortestingdev2022@gmail.com>
2024-03-09 13:50:28 +0200

    message

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QButtonGroup, QPushButton,QVBoxLayout,QHBoxLayout
+from PyQt5.QtCore import Qt  
+
 app = QApplication([])
 window = QWidget()
 window.resize(600, 500)
 window.setWindowTitle("Login")
 window.show()
+
+
+main_line = QVBoxLayout()
+line1 = QHBoxLayout()
+button1 = QPushButton("login")
+
+
+window.setLayout(line1)
+
+line1.addWidget(button1, alignment=Qt.AlignCenter)
+
+
+
 
 app.exec_()
